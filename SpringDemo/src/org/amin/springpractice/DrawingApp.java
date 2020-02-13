389302c196af8dd344324adc2e3a6c2d3cd06b67
aminul <aminul@dohatec.com.bd>
2020-02-13 11:47:30 +0600

    First Commit

---
+++ SpringDemo/src/org/amin/springpractice/DrawingApp.java
@@ -0,0 +1,30 @@
+package org.amin.springpractice;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+//		Triangle triangle = new Triangle();
+//		triangle.draw();
+		
+//		Writing Code Using the Bean Factory
+//		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+//		Triangle triangle =  (Triangle) factory.getBean("triangle");
+//		triangle.draw();
+		
+//		ApplicationContext [is a big brother of BeanFactory] and Property Initialization
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) applicationContext.getBean("triangle");
+		triangle.draw();
+		
+		
+		
+		
+	}
+
+}
